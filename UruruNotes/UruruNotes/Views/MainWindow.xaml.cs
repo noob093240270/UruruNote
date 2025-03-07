d327637d468cc675855994490d53dbd83de3f87a
m1kraze <olenyak.roman@mail.ru>
2025-03-07 21:59:19 +0300

    починил открытие закрытие слева
    починил открытие закрытие слева

--- UruruNotes/UruruNotes/Views/MainWindow.xaml.cs
+++ UruruNotes/UruruNotes/Views/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using UruruNote.Views;
 using UruruNote.ViewsModels;
 using UruruNotes.Models;
@@ -74,18 +75,24 @@ namespace UruruNotes.Views
         /// <summary>
         /// Обработчик для сворачивания и разворачивания TreeView
         /// </summary>
+        private bool isMenuOpen = false;
+
         private void ToggleVisibilityButton_Click(object sender, RoutedEventArgs e)
         {
-            var button = sender as Button;
-
-            if (TreeViewGrid.Visibility == Visibility.Visible)
+            if (isMenuOpen)
             {
-                TreeViewGrid.Visibility = Visibility.Collapsed;
+                BeginStoryboard((Storyboard)FindResource("ClosingLeftMenu"));
+                ButtonClose.Visibility = Visibility.Collapsed;
+                ButtonOpen.Visibility = Visibility.Visible;
             }
             else
             {
-                TreeViewGrid.Visibility = Visibility.Visible;
+                BeginStoryboard((Storyboard)FindResource("OpeningLeftMenu"));
+                ButtonClose.Visibility = Visibility.Visible;
+                ButtonOpen.Visibility = Visibility.Collapsed;
             }
+
+            isMenuOpen = !isMenuOpen;
         }
 
         /// <summary>
