3b2d5b20be8b9877525d23959d96195eb5bfcfe4
Timofey Batrakov <bat_tim_nik_2006@mail.ru>
2025-04-30 20:46:55 +0300

    The correct TextBox extension has been configured in the notes and reminders editing field.

---
+++ UruruNotes/UruruNotes/Models/MathConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace UruruNotes.Models
+{
+    public class MathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double height && double.TryParse(parameter?.ToString(), out double offset))
+            {
+                return height + offset; // Вычитание: height + (-90)
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
